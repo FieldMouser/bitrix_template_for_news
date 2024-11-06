8ad41cf93e223f692475273817b32acc4beb551a
FieldMouser <vlad01.com98@gmail.com>
2024-11-06 14:32:40 +0700

    Deleted tasks-for-trainees folder

--- template.php
+++ template.php
@@ -1,49 +1,41 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<?php
-// Подключаем необходимые стили и скрипты
-$this->addExternalCss($templateFolder . "/style.css");
-?>
+<?php $this->addExternalCss($templateFolder . "/tasks-for-trainees/build/css/common.css"); ?>
+<link rel="shortcut icon" href="<?= $templateFolder ?>/build/images/favicon.604825ed.ico" type="image/x-icon">
 
-
-<div class="news-list">
-    <h2>Новости</h2>
-
-    <?php if (count($arResult["ITEMS"]) > 0): ?>
-        <div class="news-items">
+<div id="barba-wrapper">
+    <div class="article-list">
+        <?php if (count($arResult["ITEMS"]) > 0): ?>
             <?php foreach ($arResult["ITEMS"] as $arItem): ?>
-                <div class="news-item">
-                    <h3 class="news-title">
-                        <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><?= $arItem["NAME"] ?></a>
-                    </h3>
-                    <div class="news-date">
-                        <?= FormatDate("d.m.Y", MakeTimeStamp($arItem["ACTIVE_FROM"])) ?>
-                    </div>
-                    <div class="news-preview-text">
-                        <?= $arItem["PREVIEW_TEXT"] ?>
-                    </div>
-                    <?php if (!empty($arItem["DETAIL_PICTURE"])): ?>
-                        <div class="news-image">
+                <a class="article-item article-list__item" href="<?= $arItem["DETAIL_PAGE_URL"] ?>" data-anim="anim-3">
+                    <div class="article-item__background">
+                        <?php if (!empty($arItem["DETAIL_PICTURE"])): ?>
                             <img src="<?= $arItem["DETAIL_PICTURE"]["SRC"] ?>" alt="<?= $arItem["NAME"] ?>">
-                        </div>
-                    <?php endif; ?>
-                </div>
+                        <?php else: ?>
+                            <img src="<?= $templateFolder ?>/build/images/default-image.jpg" alt="Нет изображения">
+                        <?php endif; ?>
+                    </div>
+                    <div class="article-item__wrapper">
+                        <div class="article-item__title"><?= $arItem["NAME"] ?></div>
+                        <div class="article-item__content"><?= $arItem["PREVIEW_TEXT"] ?></div>
+                    </div>
+                </a>
             <?php endforeach; ?>
-        </div>
-        
-        <div class="pagination">
-            <?php
-            $APPLICATION->IncludeComponent(
-                "bitrix:system.pagenavigation",
-                "",
-                array(
-                    "NAV_OBJECT" => $arResult["NAV_RESULT"],
-                    "SEF_MODE" => "N"
-                )
-            );
-            ?>
-        </div>
-    <?php else: ?>
-        <p>Новости не найдены.</p>
-    <?php endif; ?>
+        <?php else: ?>
+            <p>Новости не найдены.</p>
+        <?php endif; ?>
+    </div>
+
+    <div class="pagination">
+        <?php
+        $APPLICATION->IncludeComponent(
+            "bitrix:system.pagenavigation",
+            "",
+            array(
+                "NAV_OBJECT" => $arResult["NAV_RESULT"],
+                "SEF_MODE" => "N"
+            )
+        );
+        ?>
+    </div>
 </div>
