aacf035db6280814e72ba8f36a2cfc1cc606ecd5
FieldMouser <vlad01.com98@gmail.com>
2024-11-06 15:12:58 +0700

    updated view of files

--- template.php
+++ template.php
@@ -1,7 +1,6 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
 <?php $this->addExternalCss($templateFolder . "/tasks-for-trainees/build/css/common.css"); ?>
-<link rel="shortcut icon" href="<?= $templateFolder ?>/build/images/favicon.604825ed.ico" type="image/x-icon">
 
 <div id="barba-wrapper">
     <div class="article-list">
@@ -9,11 +8,27 @@
             <?php foreach ($arResult["ITEMS"] as $arItem): ?>
                 <a class="article-item article-list__item" href="<?= $arItem["DETAIL_PAGE_URL"] ?>" data-anim="anim-3">
                     <div class="article-item__background">
-                        <?php if (!empty($arItem["DETAIL_PICTURE"])): ?>
-                            <img src="<?= $arItem["DETAIL_PICTURE"]["SRC"] ?>" alt="<?= $arItem["NAME"] ?>">
-                        <?php else: ?>
-                            <img src="<?= $templateFolder ?>/build/images/default-image.jpg" alt="Нет изображения">
-                        <?php endif; ?>
+                        <?php
+                            // Проверяем поле DETAIL_PICTURE или PREVIEW_PICTURE для изображения
+                            $imagePath = '';
+                            
+                            // Если есть DETAIL_PICTURE (картинка для детали новости)
+                            if (!empty($arItem["DETAIL_PICTURE"])) {
+                                $imagePath = CFile::GetPath($arItem["DETAIL_PICTURE"]);
+                            }
+                            // Если нет DETAIL_PICTURE, проверяем PREVIEW_PICTURE
+                            elseif (!empty($arItem["PREVIEW_PICTURE"])) {
+                                $imagePath = CFile::GetPath($arItem["PREVIEW_PICTURE"]);
+                            }
+                            
+                            // Если изображения есть, выводим его
+                            if ($imagePath) {
+                                echo '<div class="news-image"><img src="' . $imagePath . '" alt="' . htmlspecialchars($arItem["NAME"]) . '"></div>';
+                            } else {
+                                // Если нет изображения, показываем картинку по умолчанию
+                                echo '<div class="news-image"><img src="' . $templateFolder . '/build/images/default-image.jpg" alt="Нет изображения"></div>';
+                            }
+                        ?>
                     </div>
                     <div class="article-item__wrapper">
                         <div class="article-item__title"><?= $arItem["NAME"] ?></div>
