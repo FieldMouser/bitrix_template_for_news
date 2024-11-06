25c3c58fd7d4ffddcfe7e7b5f0302527641a5cd0
FieldMouser <vlad01.com98@gmail.com>
2024-11-06 15:43:12 +0700

    fixedd view of images

--- template.php
+++ template.php
@@ -7,29 +7,14 @@
         <?php if (count($arResult["ITEMS"]) > 0): ?>
             <?php foreach ($arResult["ITEMS"] as $arItem): ?>
                 <a class="article-item article-list__item" href="<?= $arItem["DETAIL_PAGE_URL"] ?>" data-anim="anim-3">
-                    <div class="article-item__background">
-                        <?php
-                            // Проверяем поле DETAIL_PICTURE или PREVIEW_PICTURE для изображения
-                            $imagePath = '';
-                            
-                            // Если есть DETAIL_PICTURE (картинка для детали новости)
-                            if (!empty($arItem["DETAIL_PICTURE"])) {
-                                $imagePath = CFile::GetPath($arItem["DETAIL_PICTURE"]);
-                            }
-                            // Если нет DETAIL_PICTURE, проверяем PREVIEW_PICTURE
-                            elseif (!empty($arItem["PREVIEW_PICTURE"])) {
-                                $imagePath = CFile::GetPath($arItem["PREVIEW_PICTURE"]);
-                            }
-                            
-                            // Если изображения есть, выводим его
-                            if ($imagePath) {
-                                echo '<div class="news-image"><img src="' . $imagePath . '" alt="' . htmlspecialchars($arItem["NAME"]) . '"></div>';
-                            } else {
-                                // Если нет изображения, показываем картинку по умолчанию
-                                echo '<div class="news-image"><img src="' . $templateFolder . '/build/images/default-image.jpg" alt="Нет изображения"></div>';
-                            }
-                        ?>
-                    </div>
+				<div class="article-item__background">
+				    <?php if ($arItem["PREVIEW_PICTURE"]): ?>
+			        	<div class="news-image">
+			            	<img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" alt="<?= htmlspecialchars($arItem["PREVIEW_PICTURE"]["ALT"]) ?>" title="<?= htmlspecialchars($arItem["PREVIEW_PICTURE"]["TITLE"]) ?>">
+		    	    	</div>
+				    	<?php endif; ?>
+					</div>
+
                     <div class="article-item__wrapper">
                         <div class="article-item__title"><?= $arItem["NAME"] ?></div>
                         <div class="article-item__content"><?= $arItem["PREVIEW_TEXT"] ?></div>
